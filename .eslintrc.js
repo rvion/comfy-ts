55dffbd3b74bef8ce675de51057bbe8eba28a3d2
rvion <vion.remi@gmail.com>
2025-05-27 07:51:33 +0200

    eslint tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,32 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        project: "tsconfig.json",
-        tsconfigRootDir: __dirname,
-        sourceType: "module",
-    },
-    plugins: ["@typescript-eslint/eslint-plugin"],
-    extends: ["eslint:recommended", "@typescript-eslint/recommended", "@typescript-eslint/recommended-requiring-type-checking", "prettier"],
-    root: true,
-    env: {
-        node: true,
-        jest: true,
-    },
-    ignorePatterns: [".eslintrc.js", "dist/", "node_modules/"],
-    rules: {
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/explicit-function-return-type": "error",
-        "@typescript-eslint/explicit-module-boundary-types": "error",
-        "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/no-unused-vars": "error",
-        "@typescript-eslint/prefer-nullish-coalescing": "error",
-        "@typescript-eslint/prefer-optional-chain": "error",
-    },
+   parser: '@typescript-eslint/parser',
+   parserOptions: {
+      project: 'tsconfig.json',
+      tsconfigRootDir: __dirname,
+      sourceType: 'module',
+   },
+   plugins: ['@typescript-eslint/eslint-plugin'],
+   extends: [
+      'eslint:recommended',
+      '@typescript-eslint/recommended',
+      '@typescript-eslint/recommended-requiring-type-checking',
+      'prettier',
+   ],
+   root: true,
+   env: {
+      node: true,
+      jest: true,
+   },
+   ignorePatterns: ['.eslintrc.js', 'dist/', 'node_modules/'],
+   rules: {
+      'no-unused-vars': 'off', // Disable the base rule as it can conflict with TypeScript's rules
+      // ---------
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'error',
+      '@typescript-eslint/prefer-optional-chain': 'error',
+   },
 }
